4e1a7edab092254c4d340bec839d2c86645231cc
林佳志乃 <twinklestaryoshino@gmail.com>
2018-06-06 18:01:45 +0900

    clickしたら”クリックされた”表示

--- app/assets/javascripts/top.js
+++ app/assets/javascripts/top.js
@@ -2,7 +2,8 @@ window.addEventListener("load",function () {
   var btn = document.querySelector("input");
 
   btn.addEventListener("click", function(){
-    console.log("Hello world");
+    var p = document.querySelector("p");
+    p.innerText = "クリックされた！！";
   });
 });
 
