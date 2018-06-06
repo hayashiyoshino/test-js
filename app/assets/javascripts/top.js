3ea18a8a788ebdc61134f2c6b9929f0137c41aeb
林佳志乃 <twinklestaryoshino@gmail.com>
2018-06-06 17:53:42 +0900

    第二引数書き換え

--- app/assets/javascripts/top.js
+++ app/assets/javascripts/top.js
@@ -1,11 +1,9 @@
 window.addEventListener("load",function () {
   var btn = document.querySelector("input");
 
-  function printHello() {
+  btn.addEventListener("click", function(){
     console.log("Hello world");
-  }
-
-  btn.addEventListener("click", printHello);
+  });
 });
 
 
